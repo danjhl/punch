5b3b1688cccebb6c912310643adba7d374e74b05
danjhl <d4njhl@gmail.com>
2019-04-16 21:38:59 +0200

    renaming

--- src/main/scala/punch/cli/Activity.scala
+++ src/main/scala/punch/cli/Activity.scala
@@ -3,10 +3,10 @@ package punch.cli
 case class Activity(
     name: String,
     project: String,
-    start: Long,
-    end: Long) {
+    from: Long,
+    to: Long) {
   
-  val seconds: Long = end - start
+  val seconds: Long = to - from
 }
 
 object Activity {
