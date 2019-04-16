432b86c02330fb6b448edd826174fdcd399a6133
danjhl <d4njhl@gmail.com>
2019-04-16 21:41:51 +0200

    write to file

--- src/main/scala/punch/cli/Persistence.scala
+++ src/main/scala/punch/cli/Persistence.scala
@@ -2,14 +2,62 @@ package punch.cli
 
 import cats.effect.IO
 import scala.util.{Try, Success}
+import java.nio.file.{Files, Paths, StandardOpenOption}
 
-object Persistence {
+trait Store {
+  def writeActivity(activity: Activity): IO[Try[Unit]]
+  def readActivities(): IO[Try[Seq[Activity]]]
+  // deleteActivities(name: String): IO[Try[Unit]]
+  // deleteProject(name: String): IO[Try[Unit]]
+}
+
+object Persistence extends Store {
+  private val file = "act.db"
+  private val activityPattern = 
+    ("""\{\s*"name"\s*:\s*"(.*?)"\s*,\s*""" +
+    """"project"\s*:\s*"(.*?)"\s*,\s*""" +
+    """"from"\s*:\s*(\d*)\s*,\s*""" +
+    """"to"\s*:\s*(\d*)\s*,\s*""" +
+    """"seconds"\s*:\s*(\d*)\}""").r
+
+  def writeActivity(activity: Activity): IO[Try[Unit]] = {
+    IO {
+      Try(Files.write(Paths.get(file),
+        convert(activity).getBytes(), StandardOpenOption.APPEND))
+    }
+  }
+
+  // TODO test
+  def convert(activity: Activity): String = {
+    val a = activity
+    s"""\n{"name": "${a.name}", "project": "${a.project}", """ +
+      s""""from": ${a.from}, "to": ${a.to}, "seconds": ${a.seconds}}"""
+  }
+
   def readActivities(): IO[Try[Seq[Activity]]] = {
-    IO { Success(Vector(
-      Activity("actA", "projectA", 1000, 1100),
-      Activity("actA", "projectA", 1000, 11000),
-      Activity("actA", "projectA", 1000, 11000),
-      Activity("actAWithALongName", "projectA", 1000, 1100000),
-    )) }
+    IO {
+      for {
+        json       <- Try(new String(Files.readAllBytes(Paths.get(file))))
+        result     <- Try(parse(json))
+        _          <- Try(result.collect { case Left(err) => scribe.error(err) })
+        activities <- Try(result.collect { case Right(a) => a })
+      } yield activities
+    }
+  }
+
+  def parseLine(line: String, index: Int): Either[String, Activity] = {
+    line match {
+      case activityPattern(name, project, from, to, _) =>
+        Right(Activity(name, project, from.toLong, to.toLong))
+      case _ =>
+        Left(s"couldn't parse line: $index")
+    }
+  }
+
+  def parse(str: String): Seq[Either[String, Activity]] = {
+    if (str.isEmpty)
+      Nil
+    else
+      str.split("\n").zipWithIndex.map(e => parseLine(e._1, e._2))
   }
 }
---
+++ src/test/scala/punch/cli/PersistenceSpec.scala
@@ -0,0 +1,47 @@
+package punch.cli
+
+import org.scalatest._
+
+class PersistenceSpec extends FunSpec with Matchers {
+  describe("Persistence#parse") {
+    it("should parse single line") {
+      val str = 
+        """{"name": "a", "project": "p", "from": 1, "to": 2, "seconds": 1}"""
+      
+      Persistence.parse(str) shouldEqual Seq(Right(Activity("a", "p", 1, 2)))
+    }
+
+    it("should parse multiple lines") {
+      val str = 
+        """{"name": "a", "project": "p", "from": 1, "to": 2, "seconds": 1}
+          |{"name": "a2", "project": "p", "from": 1, "to": 2, "seconds": 1}"""
+            .stripMargin
+      
+      Persistence.parse(str) shouldEqual Seq(Right(Activity("a", "p", 1, 2)),
+                                             Right(Activity("a2", "p", 1, 2)))
+    }
+
+    it("should return single left") {
+      val str = """{"name": "a", "project": "p", """
+      
+      Persistence.parse(str) shouldEqual Seq(Left("couldn't parse line: 0"))
+    }
+
+    it("should return left") {
+       val str = 
+        """{"name": "a", "project": "p", "from": 1, "to": 2, "seconds": 1}
+          |{"name": "a2", "project": "p", "from": 1, "to": 2, seconds": 1}"""
+            .stripMargin
+      
+      Persistence.parse(str) shouldEqual Seq(Right(Activity("a", "p", 1, 2)),
+                                             Left("couldn't parse line: 1"))
+    }
+  }
+
+  describe("Persistence#convert") {
+    it("should convert activity") {
+      val a = Activity("name", "project", 1, 2)
+      Persistence.convert(a) shouldEqual "\n" + """{"name": "name", "project": "project", "from": 1, "to": 2, "seconds": 1}"""
+    }
+  }
+}
