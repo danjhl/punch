96c8664569f35986e5a7cf2bcdc4b4d1c43f88db
danjhl <d4njhl@gmail.com>
2020-12-17 20:02:10 +0100

    update zio dependency

--- project/Deps.scala
+++ project/Deps.scala
@@ -4,7 +4,7 @@ object Deps {
   lazy val scalaTest   = "org.scalatest"        %% "scalatest"            % "3.0.5"
   lazy val scalaMock   = "org.scalamock"        %% "scalamock"            % "4.4.0"
   lazy val scribe      = "com.outr"             %% "scribe"               % "2.7.3"
-  lazy val zio         = "dev.zio"              %% "zio"                  % "1.0.0-RC18-2"
+  lazy val zio         = "dev.zio"              %% "zio"                  % "1.0.3"
   lazy val fastparse   = "com.lihaoyi"          %% "fastparse"            % "2.1.0"
   lazy val jlineNative = "org.jline"            %  "jline"                % "3.1.3"
   lazy val jline       = "org.jline"            %  "jline"                % "3.14.0"
--- src/main/scala/punch/Main.scala
+++ src/main/scala/punch/Main.scala
@@ -1,13 +1,14 @@
 package punch
 
 import punch.cli.Cli
-import zio.{App, ZIO, IO}
+import zio.{ZIO, IO}
 
-object Main extends App {
+object Main extends zio.App {
   override def run(args: List[String]) = {
     Cli
       .interpret(args)
       .catchAll(err => IO { scribe.error(err) })
       .fold(_ => 1, _ => 0)
+      .exitCode
   }
 }
